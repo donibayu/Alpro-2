ec6e6c86e37ed4718ac65b4ea86347e54bb6c46a
doni bayu <32575733+donibayu@users.noreply.github.com>
2017-10-06 22:54:43 +0700

    Add files via upload

---
+++ Pertemuan 2/Langkah-langkah Memasak nasi.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main() {
+
+cout << "============================" <<endl;
+cout << "Langkah-langkah Memasak nasi" <<endl;
+cout << "============================" <<endl;
+cout << "1.Ambil wadah" <<endl;
+cout << "2.Ambil beras dengan gelas dua cangkir " <<endl;
+cout << "3.Setalah itu cuci beras dengan air hingga bersih " <<endl;
+cout << "4.Setelah itu cuci kembali beras dengan air lagi " <<endl;
+cout << "5.Lalu masukan air dengan ukuran tertentu " <<endl;
+cout << "6.lalu masukan wadah yang berisi beras dan air ke dalam ricecooker " <<endl;
+cout << "7.Selanjutnya tekan tombol led indikator " <<endl;
+cout << "8.Tunggu hingga tombol warm berwarna merah " <<endl;
+cout << "9.Dan nasi sudah matang dan siap di hidangkan " <<endl;
+
+
+return 0;
+}
