6d26fcd7f0c15d12b288b874832d015c049f23b7
doni bayu <32575733+donibayu@users.noreply.github.com>
2017-10-24 23:33:25 +0700

    Add files via upload

---
+++ genapdanganjil.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+int bil;
+
+cout <<"Masukan bilangan bulat : ";
+cin >>bil;
+
+if(bil%2==0) {
+cout <<bil<<"Adalah bilangan genap";
+}
+
+else {
+cout <<bil<<"Adalah bilangan ganjil";
+}
+
+return 0;
+}
